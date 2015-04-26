978b2212b07c4d9d66ee25d97eccdcb374f6839d
nuplex <nokohc@gmail.com>
2015-04-25 21:51:58 -0400

    Camera Controls, Panning

--- Assets/Scripts/CameraScripts/CameraController.cs
+++ Assets/Scripts/CameraScripts/CameraController.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainCameraControll : MonoBehaviour {
+public class CameraController : MonoBehaviour {
+	Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-	
+		offset = new Vector3 (0.0f, 0.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void LateUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		offset = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		transform.position += offset;
+
+		/*if (Input.GetKey ("w")) {
+			offset += Inp
+		} else if (Input.GetKey ("a")) {
+			
+		} else if (Input.GetKey ("s")) {
+			
+		} else if (Input.GetKey ("d")) {
+			
+		}*/
 	}
 }
