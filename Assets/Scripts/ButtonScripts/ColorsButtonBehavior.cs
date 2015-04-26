2b76ad408c20fe9371d3adc3e0174401e2fb6750
Freestate17 <daniel.lins.dm@gmail.com>
2015-04-25 23:28:14 -0400

    added manager, updated buttons/dropdown

---
+++ Assets/Scripts/ButtonScripts/ColorsButtonBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorsButtonBehavior : MonoBehaviour {
+
+	public GameObject ColorsPanel;
+
+	// show or hide the colors panel
+	public void toggleColorsPanel() {
+		ColorsPanel.SetActive (!ColorsPanel.activeSelf);
+	}
+}
