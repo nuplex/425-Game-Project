9a29bc64fdacf0273f47dcb62ac6c2b68a86b62a
Freestate17 <daniel.lins.dm@gmail.com>
2015-04-29 19:36:25 -0400

    ???

---
+++ Assets/Scripts/ButtonScripts/ButtonPanelBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPanelBehavior : MonoBehaviour {
+
+	public GameObject ColorsPanel;
+	public Texture2D pathTexture, destroyTexture, defaultTexture;
+
+	// show or hide the colors panel
+	public void toggleColorsPanel() {
+		ColorsPanel.SetActive (!ColorsPanel.activeSelf);
+	}
+
+	public void setCursorToPathIcon() {
+		Cursor.SetCursor (pathTexture, new Vector2 (256, 256), CursorMode.Auto);
+	}
+
+	public void setCursorToDestroyIcon() {
+		Cursor.SetCursor (destroyTexture, new Vector2(256, 256), CursorMode.Auto);
+	}
+
+	public void setCursorToDefaultIcon() {
+		Cursor.SetCursor (defaultTexture, new Vector2(65, 65), CursorMode.Auto);
+	}
+}
