e5f2c8989a0c9935dd51a48ab811c1ce2a1615f5
Freestate17 <daniel.lins.dm@gmail.com>
2015-04-29 18:09:44 -0400

    changed default cursor texture

---
+++ Assets/Scripts/CursorScripts/CursorHideShow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorHideShow : MonoBehaviour {
+	bool isVisible;
+	public Texture2D defaultCursor;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.SetCursor (defaultCursor, new Vector2(65, 65), CursorMode.Auto);
+		Cursor.visible = true;
+		isVisible = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.V)) {
+			isVisible = !isVisible;
+			Cursor.visible = isVisible;
+		}
+	}
+}
