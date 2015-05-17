5dee36a15c172e25d92add152248235913c9c7fe
Freestate17 <daniel.lins.dm@gmail.com>
2015-05-17 01:02:35 -0400

    ?

--- Assets/Scripts/CameraScripts/CameraManager.cs
+++ Assets/Scripts/CameraScripts/CameraManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class CameraManager : MonoBehaviour {
 	public Camera camNE;
@@ -26,15 +27,22 @@ public class CameraManager : MonoBehaviour {
 	private const int CAM_NE = 1, CAM_SE = 2, CAM_SW = 3, CAM_NW = 4;
 
 	private bool naturalMode;
-	public bool placing;
+	bool placing;
+	bool destroying;
 
+	public GameObject highlight;
 	public GameObject plane;
 	public GameObject cubeMaster;
 	GameObject cube;
+	Vector3 p;
+
+	private GameObject[,] grid;
 
 	// Use this for initialization
 	void Start () {
+		grid = new GameObject[100, 100];
 		placing = false;
+		destroying = false;
 		naturalMode = true;
 		currentCam = 0;
 		activeCamera = camSW;
@@ -46,16 +54,27 @@ public class CameraManager : MonoBehaviour {
 		naturalSet = new Camera[4]{camSW, camSE, camNE, camNW};
 		unnaturalSet = new Camera[4]{camNE, camSE, camSW, camNW};
 		offset = new Vector3 (0.0f, 0.0f, 0.0f);
+		p = new Vector3 (0,0,0);
 		setDirections ();
+	}
+
+	void resetHighlight () {
+		highlight.transform.position = new Vector3 (0,-1,0);
 	}
 
 	public void setPlacing (bool placingIn) {
 		placing = placingIn;
 	}
 
+	public void setDestroying (bool destroyingIn) {
+		destroying = destroyingIn;
+		resetHighlight ();
+	}
+
 	public void newCube () {
-		cube = (GameObject) Instantiate (cubeMaster);
-		cube.transform.position = new Vector3 (5, 5, 5);
+		if (!placing) {
+			cube = (GameObject) Instantiate (cubeMaster);
+		}
 	}
 
 	// sets movement directions for all four cameras based on which one is active
@@ -67,30 +86,82 @@ public class CameraManager : MonoBehaviour {
 		backward = -forward;
 	}
 
+	// if the mouse is on a grid cell, returns true and sets p to the world coordinates cell
+	// returns false if the mouse is not on a grid cell
+	bool raycast () {
+		RaycastHit hitInfo;
+		float cellWidth = 1.0f;
+		if (Physics.Raycast (activeCamera.transform.position, (activeCamera.ScreenPointToRay (Input.mousePosition).direction), out hitInfo)) {
+			Vector3 pivotToPoint = hitInfo.point - plane.transform.position;
+			float tileX = pivotToPoint.x / cellWidth;
+			float tileZ = pivotToPoint.z / cellWidth;
+			tileX = Mathf.Floor (tileX);
+			tileZ = Mathf.Floor (tileZ);
+			float worldX = plane.transform.position.x + tileX * cellWidth + cellWidth / 2.0f;
+			float worldZ = plane.transform.position.z + tileZ * cellWidth + cellWidth / 2.0f;
+			p = new Vector3(worldX, 0, worldZ);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	void placeObject () {
-		if (placing) {
-			RaycastHit hit;
-			float cellWidth = 1.0f;
-			if (Physics.Raycast (activeCamera.transform.position, (activeCamera.ScreenPointToRay (Input.mousePosition).direction), out hit)) {
-				Vector3 pivotToPoint = hit.point - plane.transform.position;
-				float tileX = pivotToPoint.x / cellWidth;
-				float tileZ = pivotToPoint.z / cellWidth;
-				tileX = Mathf.Floor(tileX);
-				tileZ = Mathf.Floor(tileZ);
-				float worldX = plane.transform.position.x + tileX * cellWidth + cellWidth / 2.0f;
-				float worldZ = plane.transform.position.z + tileZ * cellWidth + cellWidth / 2.0f;
-				Debug.DrawLine(new Vector3 (0,0,0), new Vector3 (worldX, 0, worldZ));
-				cube.transform.position = new Vector3(worldX, 0.5f, worldZ);
+		if (raycast () && !EventSystem.current.IsPointerOverGameObject()) {
+			Cursor.visible = false;
+			Debug.DrawLine (new Vector3 (0, 0, 0), new Vector3 (p.x, 0, p.z));
+			highlight.transform.position = new Vector3 (p.x, 0.005f, p.z);
+			if (Input.GetMouseButtonUp (0)) {
+				int gridX = (int)(p.x+49.5);
+				int gridZ = (int)(p.z+49.5);
+				if (grid[gridX,gridZ] == null) {
+					cube = (GameObject) Instantiate (cubeMaster);
+					cube.transform.position = new Vector3 (p.x, 0.05f, p.z);
+					grid[gridX,gridZ] = cube;
+				}
 			}
+		} else {
+			Cursor.visible = true;
+			resetHighlight ();
+			/*p = activeCamera.ScreenPointToRay(Input.mousePosition);
+			cube.transform.position = activeCamera.transform.position + p.direction * 20;*/
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			placing = false;
+			Cursor.visible = true;
+			resetHighlight ();
+			Destroy (cube);
+		}
+		
+	}
+
+	void destroyObject () {
+		if (raycast () && !EventSystem.current.IsPointerOverGameObject()) {
+			Cursor.visible = false;
+			highlight.transform.position = new Vector3 (p.x, 0.005f, p.z);
+			if (Input.GetMouseButtonUp (0)) {
+				int gridX = (int)(p.x + 49.5);
+				int gridZ = (int)(p.z + 49.5);
+				if (grid [gridX, gridZ] != null) {
+					Destroy (grid [gridX, gridZ]);
+					grid [gridX, gridZ] = null;
+				}
+			}
+		} else {
+			resetHighlight ();
+			Cursor.visible = true;
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		placing = true;
-		placeObject ();
-
+		if (placing) {
+			placeObject ();
+		}
+		if (destroying) {
+			destroyObject ();
+		}
 		if (Input.GetKeyDown ("q")) {
 			ChangeCam(PrevCam ());
 			if(naturalMode){
@@ -241,7 +312,22 @@ public class CameraManager : MonoBehaviour {
 	}
 
 	void Zoom(bool zoomIn){
-		for (int i = 0; i < cams.Length; i++) {
+		if (zoomIn) {
+			for (int i = 0; i < cams.Length; i++) {
+				//cams [i].transform.Translate (activeCamera.transform.forward * zoomSpeed * Time.deltaTime, Space.World);
+				Vector3 newPosition = cams[i].transform.position + activeCamera.transform.forward * zoomSpeed;
+				if (newPosition.y >= 1)
+					cams [i].transform.position = cams[i].transform.position + activeCamera.transform.forward * zoomSpeed;
+			}
+		} else if (!zoomIn){
+			for (int i = 0; i < cams.Length; i++) {
+				//cams [i].transform.Translate (-(activeCamera.transform.forward) * zoomSpeed * Time.deltaTime, Space.World);
+				Vector3 newPosition = cams[i].transform.position + activeCamera.transform.forward * zoomSpeed;
+				if (newPosition.y <= Mathf.Infinity)
+					cams [i].transform.position = cams[i].transform.position + -(activeCamera.transform.forward) * zoomSpeed;
+			}
+		}
+		/*for (int i = 0; i < cams.Length; i++) {
 			if (zoomIn) {
 				if (cams[i].fieldOfView >= minZoomFOV) {
 					cams[i].fieldOfView -= zoomSpeed / 8;
@@ -251,6 +337,6 @@ public class CameraManager : MonoBehaviour {
 					cams[i].fieldOfView += zoomSpeed / 8;
 				}
 			}
-		}
+		}*/
 	}
 }
