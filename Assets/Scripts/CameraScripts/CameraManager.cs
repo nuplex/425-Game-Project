53d0dc693cd74bdcb4d0a075f37d5017ebd599c2
Freestate17 <daniel.lins.dm@gmail.com>
2015-05-19 00:47:52 -0400

    path refreshing!!!!!!!

--- Assets/Scripts/CameraScripts/CameraManager.cs
+++ Assets/Scripts/CameraScripts/CameraManager.cs
@@ -209,6 +209,118 @@ public class CameraManager : MonoBehaviour {
 		}
 	}
 
+	string pathType(int gridX, int gridZ) {
+		GameObject curr = grid [gridX, gridZ];
+		if (curr == null ||
+			curr.tag != "Red Gro A" && curr.tag != "Red Gro AA" && curr.tag != "Red Gro AAA" &&
+			curr.tag != "Blue Gro A" && curr.tag != "Blue Gro AA" && curr.tag != "Blue Gro AAA" &&
+			curr.tag != "Green Gro A" && curr.tag != "Green Gro AA" && curr.tag != "Green Gro AAA" &&
+			curr.tag != "Yellow Gro") {
+			int left = gridX - 1;
+			int right = gridX + 1;
+			int top = gridZ + 1;
+			int bottom = gridZ - 1;
+			//all
+			if (grid [left, gridZ] != null && grid [right, gridZ] != null && grid [gridX, top] != null && grid [gridX, bottom] != null) {
+				return "path_a";
+				//3t
+			} else if (grid [left, gridZ] != null && grid [right, gridZ] != null && grid [gridX, top] != null) {
+				return "path_3t";
+				//3b
+			} else if (grid [left, gridZ] != null && grid [right, gridZ] != null && grid [gridX, bottom] != null) {
+				return "path_3b";
+				//3l
+			} else if (grid [left, gridZ] != null && grid [gridX, top] != null && grid [gridX, bottom] != null) {
+				return "path_3l";
+				//3r
+			} else if (grid [right, gridZ] != null && grid [gridX, top] != null && grid [gridX, bottom] != null) {
+				return "path_3r";
+				//ttl
+			} else if (grid [left, gridZ] != null && grid [gridX, top] != null) {
+				return "path_ttl";
+				//ttr
+			} else if (grid [right, gridZ] != null && grid [gridX, top]) {
+				return "path_ttr";
+				//btl
+			} else if (grid [left, gridZ] != null && grid [gridX, bottom] != null) {
+				return "path_btl";
+				//btr
+			} else if (grid [right, gridZ] != null && grid [gridX, bottom] != null) {
+				return "path_btr";
+				//h
+			} else if (grid [right, gridZ] != null || grid [left, gridZ] != null) {
+				return "path_h";
+				//v
+			} else if (grid [gridX, top] != null || grid [gridX, bottom] != null) {
+				return "path_v";
+				//default
+			} else {
+				return "path_default";
+			}
+		} else {
+			return "cube";
+		}
+	}
+
+	//TODO check to see if this is actually a path!!!
+	void refreshPaths (int gridX, int gridZ) {
+		GameObject curr = grid [gridX, gridZ];
+		if (curr != null) {
+			switch (pathType (gridX, gridZ)) {
+			case "path_a":
+				curr.GetComponent<Renderer> ().material = pathA;
+				curr.tag = "path_a";
+				break;
+			case "path_3t":
+				curr.GetComponent<Renderer> ().material = path3T;
+				curr.tag = "path_3t";
+				break;
+			case "path_3b":
+				curr.GetComponent<Renderer> ().material = path3B;
+				curr.tag = "path_3b";
+				break;
+			case "path_3l":
+				curr.GetComponent<Renderer> ().material = path3L;
+				curr.tag = "path_3l";
+				break;
+			case "path_3r":
+				curr.GetComponent<Renderer> ().material = path3R;
+				curr.tag = "path_3r";
+				break;
+			case "path_ttl":
+				curr.GetComponent<Renderer> ().material = pathTTL;
+				curr.tag = "path_ttl";
+				break;
+			case "path_ttr":
+				curr.GetComponent<Renderer> ().material = pathTTR;
+				curr.tag = "path_ttr";
+				break;
+			case "path_btl":
+				curr.GetComponent<Renderer> ().material = pathBTL;
+				curr.tag = "path_btl";
+				break;
+			case "path_btr":
+				curr.GetComponent<Renderer> ().material = pathBTR;
+				curr.tag = "path_btr";
+				break;
+			case "path_h":
+				curr.GetComponent<Renderer> ().material = pathH;
+				curr.tag = "path_h";
+				break;
+			case "path_v":
+				curr.GetComponent<Renderer> ().material = pathV;
+				curr.tag = "path_v";
+				break;
+			case "path_default":
+				curr.GetComponent<Renderer> ().material = pathDefault;
+				curr.tag = "path_default";
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
 	void path () {
 		if (raycast () && !EventSystem.current.IsPointerOverGameObject ()) {
 			Cursor.visible = false;
@@ -219,84 +331,64 @@ public class CameraManager : MonoBehaviour {
 				int gridX = (int)(p.x+49.5);
 				int gridZ = (int)(p.z+49.5);
 				if (grid[gridX,gridZ] == null) {
-					int left = gridX - 1;
-					int right = gridX + 1;
-					int top = gridZ + 1;
-					int bottom = gridZ - 1;
 					cube.GetComponent<MeshCollider>().enabled = true;
-					//all
-					if (grid[left,gridZ] != null && grid[right,gridZ] != null && grid[gridX,top] != null && grid[gridX,bottom] != null) {
+
+					switch (pathType (gridX, gridZ)) {
+					case "path_a":
 						cube.GetComponent<Renderer>().material = pathA;
 						cube.tag = "path_a";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//3t
-					} else if (grid[left,gridZ] != null && grid[right,gridZ] != null && grid[gridX,top] != null) {
+						break;
+					case "path_3t":
 						cube.GetComponent<Renderer>().material = path3T;
 						cube.tag = "path_3t";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//3b
-					} else if (grid[left,gridZ] != null && grid[right,gridZ] != null && grid[gridX,bottom] != null) {
+						break;
+					case "path_3b":
 						cube.GetComponent<Renderer>().material = path3B;
 						cube.tag = "path_3b";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//3l
-					} else if (grid[left,gridZ] != null && grid[gridX,top] != null && grid[gridX,bottom] != null) {
+						break;
+					case "path_3l":
 						cube.GetComponent<Renderer>().material = path3L;
 						cube.tag = "path_3l";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//3r
-					} else if (grid[right,gridZ] != null && grid[gridX,top] != null && grid[gridX,bottom] != null) {
+						break;
+					case "path_3r":
 						cube.GetComponent<Renderer>().material = path3R;
 						cube.tag = "path_3r";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//ttl
-					} else if (grid[left,gridZ] != null && grid[gridX,top] != null) {
+						break;
+					case "path_ttl":
 						cube.GetComponent<Renderer>().material = pathTTL;
 						cube.tag = "path_ttl";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//ttr
-					} else if (grid[right,gridZ] != null && grid[gridX,top]) {
+						break;
+					case "path_ttr":
 						cube.GetComponent<Renderer>().material = pathTTR;
 						cube.tag = "path_ttr";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//btl
-					} else if (grid[left,gridZ] != null && grid[gridX,bottom] != null) {
+						break;
+					case "path_btl":
 						cube.GetComponent<Renderer>().material = pathBTL;
 						cube.tag = "path_btl";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//btr
-					} else if (grid[right,gridZ] != null && grid[gridX,bottom] != null) {
+						break;
+					case "path_btr":
 						cube.GetComponent<Renderer>().material = pathBTR;
 						cube.tag = "path_btr";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//h
-					} else if (grid[right,gridZ] != null || grid[left,gridZ] != null) {
+						break;
+					case "path_h":
 						cube.GetComponent<Renderer>().material = pathH;
 						cube.tag = "path_h";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//v
-					} else if (grid[gridX,top] != null || grid[gridX,bottom] != null) {
+						break;
+					case "path_v":
 						cube.GetComponent<Renderer>().material = pathV;
 						cube.tag = "path_v";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
-					//default
-					} else {
+						break;
+					case "path_default":
 						cube.GetComponent<Renderer>().material = pathDefault;
 						cube.tag = "path_default";
-						grid[gridX,gridZ] = cube;
-						cube = (GameObject) Instantiate (pathMaster);
+						break;
 					}
+					grid[gridX,gridZ] = cube;
+					refreshPaths (gridX-1, gridZ);
+					refreshPaths (gridX+1, gridZ);
+					refreshPaths (gridX, gridZ+1);
+					refreshPaths (gridX, gridZ-1);
+					cube = (GameObject) Instantiate (pathMaster);
 					//TODO check surrounding cells to see 
 				}
 			}
@@ -342,6 +434,10 @@ public class CameraManager : MonoBehaviour {
 					cube.GetComponent<GroScript>().placed = true;
 					//cube.transform.position = new Vector3 (p.x, 0.05f, p.z);
 					grid[gridX,gridZ] = cube;
+					refreshPaths(gridX-1,gridZ);
+					refreshPaths(gridX+1,gridZ);
+					refreshPaths(gridX,gridZ+1);
+					refreshPaths(gridX,gridZ-1);
 					cube = (GameObject) Instantiate (cubeMaster);
 				}
 			}
@@ -427,6 +523,10 @@ public class CameraManager : MonoBehaviour {
 				if (Input.GetMouseButtonUp (0)) {
 					Destroy (grid [gridX, gridZ]);
 					grid [gridX, gridZ] = null;
+					refreshPaths(gridX-1,gridZ);
+					refreshPaths(gridX+1,gridZ);
+					refreshPaths(gridX,gridZ+1);
+					refreshPaths(gridX,gridZ-1);
 				}
 			} else {
 				Cursor.visible = true;
