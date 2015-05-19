0f97778e9f8e50c8614bcd609b73a864d8825e63
nuplex <nokohc@gmail.com>
2015-05-19 19:56:00 -0400

    Made Menu

---
+++ Assets/Scripts/GroBlopScripts/BlopPoints.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlopPoints : MonoBehaviour {
+
+	public UnityEngine.UI.Text bpText;
+	private const string bp = "BP: ";
+	long points;
+
+	// Use this for initialization
+	void Start () {
+		points = 100;
+		UpdateText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("h")) {
+			Add (10000000);
+		}
+	}
+
+	public bool CanBuy(long amt){
+		if (points - amt < 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public void Use(long amt){
+		if (points - amt < 0) {
+			return;
+		}
+		points -= amt;
+		UpdateText ();
+	}
+
+	public void Add(long amt){
+		points += amt;
+		UpdateText ();
+	}
+
+	void UpdateText(){
+		bpText.text = bp + points;
+	}
+}
---
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public UnityEngine.UI.Button newGame;
+	public UnityEngine.UI.Button continueGame;
+
+	bool gameExists; 
+
+	// Use this for initialization
+	void Start () {
+		if (!GameFileExists ()) {
+			continueGame.interactable = false;
+		}
+	}
+
+	bool GameFileExists(){
+		return false;
+	}
+
+		
+	public void NewGame(){
+		Application.LoadLevel (1);
+	}
+
+	public void LoadGame(){
+		Application.LoadLevel (1);
+	}
+}
